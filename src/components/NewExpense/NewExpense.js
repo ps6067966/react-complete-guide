727437c28570b0a8041cdd0e9b55791064ca9cbb
Pratap Singh <ps6067966@gmail.com>
2022-10-08 17:23:39 +0530

    ps

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [IsAddNewExpenseButtonVisible, setIsAddNewExpenseButtonVisible] =
+    useState(true);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpenseData(expenseData);
+    setIsAddNewExpenseButtonVisible(true);
   };
+
+  const startEditingHandler = () => {
+    setIsAddNewExpenseButtonVisible(false);
+  };
+
+  const stopEditingHandler = () => {
+    setIsAddNewExpenseButtonVisible(true);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {IsAddNewExpenseButtonVisible && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {!IsAddNewExpenseButtonVisible && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
